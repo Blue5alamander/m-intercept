c9b1bbdf8c968c4c32bdffc12ece1aa2abd07980
Kirit Sælensminde <k@kirit.com>
2010-03-14 11:55:42 +0000

    Changed a number of occurrences of AnimRay to Missile intercept.

--- android/src/com/kirit/android/mintercept/views/Scene.java
+++ android/src/com/kirit/android/mintercept/views/Scene.java
@@ -4,12 +4,12 @@
 
     This file is part of Missile intercept.
 
-    AnimRay is free software: you can redistribute it and/or modify
+    Missile intercept is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    AnimRay is distributed in the hope that it will be useful,
+    Missile intercept is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
