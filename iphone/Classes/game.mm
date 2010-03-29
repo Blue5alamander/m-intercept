a385e4d660ee6954472e9da0106ee738f780d27a
Kirit Sælensminde <k@kirit.com>
2010-03-29 12:19:14 +0000

    Added new game class.

---
+++ iphone/Classes/game.mm
@@ -0,0 +1,11 @@
+/*
+ *  game.mm
+ *  Missile-intercept
+ *
+ *  Created by Kirit Saelensminde on 29/03/2010.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "game.h"
+
